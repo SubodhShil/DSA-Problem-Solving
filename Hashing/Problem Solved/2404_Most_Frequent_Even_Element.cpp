298a11030b27528061e4e01973462e7a8e0c67d3
HomieSubodh <golang602@gmail.com>
2023-03-25 11:57:49 +0600

    March 25, 2023 (3)

--- Hashing/Problem Solved/2404_Most_Frequent_Even_Element.cpp
+++ Hashing/Problem Solved/2404_Most_Frequent_Even_Element.cpp
@@ -14,28 +14,42 @@ class Solution
 public:
     int mostFrequentEven(vector<int> &nums)
     {
-        int result = -1;
+        int frequency = 0;
+        int value = INT_MAX;
 
-        /// @dev unordered_map has taken to reduce T.C from O(N) to O(1)
+        /// @dev unordered_map has taken for reducing T.C from O(N) to O(1)
         unordered_map<int, int> hashing;
-        for (auto i : nums)
-            hashing[i]++;
-
-        vector<pair<int, int>> temp;
-        for (auto &it : hashing)
+        for (auto &i : nums)
         {
-            temp.push_back(make_pair(it.second, it.first));
+            /// @check:     whether current element is even
+            /// @intuition: filter out only even numbers at the time of hashing
+            /// @concept:   check even using bit manipulation
+            ///             i % 2 == 0 is same as !(i & 1) or (i & 1) == 0
+            if (!(i & 1))
+            {
+                /// storing frequency in the hashing
+                hashing[i]++;
+
+                if (hashing[i] > frequency || (hashing[i] == frequency && i < value))
+                {
+                    value = i;
+                    frequency = hashing[i];
+                }
+            }
         }
 
-        /// we've to return the most frequent element
-        sort(temp.begin(), temp.end(), greater<>());
-        
-        return result;
+        return (frequency == 0) ? -1 : value;
     }
 };
 
 int main()
 {
+    /// test cases
+    // vector<int> nums = {0, 1, 2, 2, 4, 4, 1};
+    // vector<int> nums = {4, 4, 4, 9, 2, 4};
+    vector<int> nums = {29, 47, 21, 41, 13, 37, 25, 7};
+    Solution op1;
+    cout << op1.mostFrequentEven(nums);
 
     return 0;
 }
