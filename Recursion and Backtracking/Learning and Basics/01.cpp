d3ed71b103b2ada5615261f07da5ab7fff985b11
HomieSubodh <golang602@gmail.com>
2023-04-01 20:13:16 +0600

    April 1 2023 (5)

---
+++ Recursion and Backtracking/Learning and Basics/01.cpp
@@ -0,0 +1,28 @@
+/// Simple recursion
+/// Recursive call should be stopped after printing
+/// "Hello world" for 5 time
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/// global variable to track the number of time
+/// it has printed "Hello world"
+int cnt = 1;
+
+void printHelloWorld()
+{
+    /// base case
+    if (cnt > 5)
+        return;
+
+    cout << "Hello world, count = " << cnt++ << "\n";
+
+    printHelloWorld();
+}
+
+int main()
+{
+    printHelloWorld();
+
+    return 0;
+}
