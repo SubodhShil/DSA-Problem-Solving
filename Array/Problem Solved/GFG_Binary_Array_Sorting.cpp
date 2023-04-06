f939f462420eee79bfd23d6a62265be1d5a59650
HomieSubodh <golang602@gmail.com>
2023-04-06 14:01:27 +0600

    April 6, 2023

--- Array/Problem Solved/GFG_Binary_Array_Sorting.cpp
+++ Array/Problem Solved/GFG_Binary_Array_Sorting.cpp
@@ -1,4 +1,6 @@
-/// https://practice.geeksforgeeks.org/problems/binary-array-sorting-1587115620/1?page=1&difficulty[]=-1&category[]=Arrays&sortBy=submissions
+/**
+ * https://practice.geeksforgeeks.org/problems/binary-array-sorting-1587115620/1?page=1&difficulty[]=-1&category[]=Arrays&sortBy=submissions
+ */
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -14,15 +16,24 @@ public:
 
         while (left <= right)
         {
-            if (A[left] > A[right])
-                swap(A[left++], A[right++]);
-            else if ()
+            if (A[left] == 1 and A[right] == 0)
+                swap(A[left++], A[right--]);
+            else if (A[left] == 0 and A[right] == 0)
+                left++;
+            else if (A[left] == 1 and A[right] == 1)
+                right--;
+            else
+                left++, right--;
         }
     }
 };
 
 int main()
 {
+    Solution sol;
+    // int A[]{1, 0, 1, 1, 1, 1, 1, 0, 0, 0};
+    int A[]{1, 0, 1, 1, 0};
+    sol.binSort(A, 5);
 
     return 0;
 }
