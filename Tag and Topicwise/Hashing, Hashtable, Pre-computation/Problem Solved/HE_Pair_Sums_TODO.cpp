8bd8370d51403d02b4246913d046309e0b66ca4a
HomieSubodh <golang602@gmail.com>
2023-04-08 23:56:57 +0600

    April 8, 2023 (1)

---
+++ Tag and Topicwise/Hashing, Hashtable, Pre-computation/Problem Solved/HE_Pair_Sums_TODO.cpp
@@ -0,0 +1,26 @@
+/// TODO
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        mp[x]++;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (mp[k - arr[i]])
+    }
+
+    return 0;
+}
---
+++ Tag and Topicwise/Recursion and Backtracking/Learning and Basics/factorial_of_N.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int N)
+{
+    if (N == 1)
+        return 1;
+
+    return factorial(N - 1) * N;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << factorial(n) << endl;
+
+    return 0;
+}
