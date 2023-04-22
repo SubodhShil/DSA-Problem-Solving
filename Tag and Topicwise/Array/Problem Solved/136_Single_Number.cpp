71d947161bca1849e41a8a6e28985f8629624f7f
HomieSubodh <golang602@gmail.com>
2023-04-22 11:51:42 +0600

    April 22, 2023 (2)

--- Tag and Topicwise/Array/Problem Solved/136_Single_Number.cpp
+++ Tag and Topicwise/Array/Problem Solved/136_Single_Number.cpp
@@ -3,7 +3,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+/// Bruteforce solution
+/// Time complexity: O(n * n)
+class Solution1
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int size = nums.size();
+
+        for (int i = 0; i < size; ++i)
+        {
+            int counter = 0;
+            int currentNumber = nums[i];
+
+            for (int j = 0; j < size; ++j)
+            {
+                if (nums[j] == currentNumber)
+                    ++counter;
+            }
+
+            if (counter == 1)
+                return currentNumber;
+        }
+
+        return -1;
+    }
+};
+
+/// Better solution
+class Solution2
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        vector<int> v = nums;
+
+        /// sort the array or list
+        sort(begin(v), end(v));
+        int n = v.size();
+
+        for (int i = 0; i < n;)
+        {
+            if (v[i] == v[i + 1])
+                i += 2;
+            else
+                return v[i];
+        }
+
+        return -1;
+    }
+};
+
+/// Optimal solution 1: using pre-computation: hashing
+class Solution3
 {
 public:
     int singleNumber(vector<int> &nums)
@@ -21,11 +74,38 @@ public:
     }
 };
 
+/// Optimal solution 2: using bit manipulation
+class Solution4
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int ans = 0;
+        for (auto i : nums)
+            ans ^= i;
+
+        return ans;
+    }
+};
+
 int main()
 {
-    Solution result;
-    vector<int> nums = {1};
-    cout << result.singleNumber(nums);
+    // vector<int> nums = {1};
+    vector<int> nums = {3, 13, 33, 13, 33, 3, 1, 1, 0};
+    // vector<int> nums = {1, 1, 2, 3, 3, 0, 0};
+
+    Solution1 result1;
+    cout << result1.singleNumber(nums);
+
+    cout << endl;
+
+    Solution2 result2;
+    cout << result2.singleNumber(nums);
+
+    cout << endl;
+
+    Solution3 result3;
+    cout << result3.singleNumber(nums);
 
     return 0;
 }
