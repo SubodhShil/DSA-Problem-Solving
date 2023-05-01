4532fbf67a5beca86d69feb00c98c000de9b0235
HomieSubodh <golang602@gmail.com>
2023-05-01 06:48:41 +0600

    Files truncated

--- Software Development - Practice and Learning/Space - Backend/April 21, 2023/Practice1/app.js
+++
@@ -1,8 +0,0 @@
-/// require express to use it
-const express = require('express');
-const app = express();
-
-/// Your application will serve at http://localhost:PORT
-const PORT = 4000;
-
-module.exports = { app, PORT };
