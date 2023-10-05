3509cc04fb09ca09860ac6d3bf7c36b8b12aa81a
Subodh Chandra Shil <golang602@gmail.com>
2023-10-05 09:00:38 +0600

    GFG_Left_most_and_right_most_index.cpp optimized solution added

--- Algorithms/Binary Search/Interview Questions/GFG_Left_most_and_right_most_index.cpp
+++ Algorithms/Binary Search/Interview Questions/GFG_Left_most_and_right_most_index.cpp
@@ -7,7 +7,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+class Solution1
 {
 public:
     pair<long, long> indexes(vector<long long> v, long long x)
@@ -49,6 +49,75 @@ public:
         }
 
         return {firstOccurrence, lastOccurrence};
+    }
+};
+
+/// Optimized solution: O(log n)
+class Solution2
+{
+    public:
+    int leftIndex(vector<long long> v, long long x)
+    {
+        int n = v.size(); 
+        int start = 0, end = n - 1;
+        
+        while(start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            
+            if(v[mid] == x)
+            {
+                int prev = (mid - 1) % n;
+                if(v[mid] != v[prev])
+                    return mid;
+                if(v[mid] == v[prev])
+                    end = mid - 1;
+            }
+            
+            else if(v[start] > v[end] && x < v[mid]
+                 || v[start] < v[end] && x > v[mid])
+                start = mid + 1;
+            else 
+                end = mid - 1;
+        }
+        
+        return -1;
+    }
+    
+    int rightIndex(vector<long long> v, long long x)
+    {
+        int n = v.size();
+        int start = 0, end = v.size() - 1;
+        
+        while(start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            int next = (mid + 1) % n;
+            
+            if(v[mid] == x)
+            {
+                if(v[mid] != v[next])
+                    return mid;
+                if(v[mid] == v[next])
+                    start = mid + 1;
+            }
+            
+            else if(v[start] > v[end] && x < v[mid]
+                 || v[start] < v[end] && x > v[mid])
+                start = mid + 1;
+            else 
+                end = mid - 1;
+        }
+        
+        return -1;
+    }
+    
+    pair<long,long> indexes(vector<long long> v, long long x)
+    {
+        int left = leftIndex(v, x);
+        int right = rightIndex(v, x);
+        
+        return {left, right};
     }
 };
 
