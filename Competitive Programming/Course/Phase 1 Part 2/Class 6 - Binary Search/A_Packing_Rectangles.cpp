a7d145610f6392d26af487cef787e160e4b18aea
Subodh Chandra Shil <golang602@gmail.com>
2024-04-30 15:40:17 +0600

    30-04-2024 init

--- Competitive Programming/Course/Phase 1 Part 2/Class 6 - Binary Search/A_Packing_Rectangles.cpp
+++ Competitive Programming/Course/Phase 1 Part 2/Class 6 - Binary Search/A_Packing_Rectangles.cpp
@@ -4,15 +4,42 @@ using namespace std;
 
 int w, h, n;
 
-bool ok(int l)
+int MAX_RANGE = 1e14;
+
+bool ok(int square_length)
 {
-    int cnt = (l / w) * (l / h);
-    return cnt >= n;
+    // int cnt = (square_length / w) * (square_length / h);
+    // return cnt >= n;
+
+    double a = (square_length / w), b = (square_length / h);
+    if (a == 0 or b == 0)
+        return false;
+
+    return a >= (n * 1.0) / (b * 1.0);
 }
 
 int32_t main()
 {
+
+    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
     cin >> w >> h >> n;
+
+    int l = 0, r = MAX_RANGE, ans = 0;
+    while (l <= r)
+    {
+        int mid = l + ((r - l) / 2);
+        if (ok(mid))
+        {
+            ans = mid;
+            r = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+
+    cout << ans << endl;
 
     return 0;
 }
