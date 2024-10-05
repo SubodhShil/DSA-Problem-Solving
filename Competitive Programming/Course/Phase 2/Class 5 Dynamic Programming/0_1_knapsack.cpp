2dcc483059f7b3f5fadb30a22e83ad0077527395
Subodh Chandra Shil <golang602@gmail.com>
2024-10-05 23:59:15 +0600

    0/1 knapsack added

--- Competitive Programming/Course/Phase 2/Class 5 Dynamic Programming/0_1_knapsack.cpp
+++ Competitive Programming/Course/Phase 2/Class 5 Dynamic Programming/0_1_knapsack.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 int n, k, m, a, b, c, weight_limit;
 const int N = 105;
-int w[N], v[N];
+int w[N], v[N], dp[N][100005];
 
 int knapsack_recursive(int i, int weight)
 {
@@ -31,17 +31,36 @@ int knapsack_recursive(int i, int weight)
     return ans;
 }
 
+int knapsack_dp(int i, int weight)
+{
+    if (i == n + 1)
+        return 0;
+    if (dp[i][weight] != -1)
+        return dp[i][weight];
+
+    // nibo na
+    int ans = knapsack_dp(i + 1, weight);
+
+    // nibo
+    if (weight + w[i] <= weight_limit)
+        ans = max(ans, knapsack_dp(i + 1, weight + w[i]) + v[i]);
+
+    return dp[i][weight] = ans;
+}
+
 int32_t main()
 {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     cin >> n >> weight_limit;
 
+    memset(dp, -1, sizeof(dp));
     for (int i = 1; i <= n; ++i)
     {
         cin >> w[i] >> v[i];
     }
 
-    cout << knapsack_recursive(1, 0) << endl;
+    // cout << knapsack_recursive(1, 0) << endl;
+    cout << knapsack_dp(1, 0) << endl;
 
     return 0;
 }
